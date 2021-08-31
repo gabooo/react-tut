c2e6a112426d675f8267bb4a83813402345a2bec
gap <gap@cloudera.com>
2021-08-31 12:42:26 +0200

    adding click handler

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,11 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = ({ title, amount, date }) => {
+
+  const clickHandler = () => {
+
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
@@ -12,6 +17,8 @@ const ExpenseItem = ({ title, amount, date }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 };
